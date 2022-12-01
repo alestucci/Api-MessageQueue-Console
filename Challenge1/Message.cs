22655603e1c271063d1ec7c21c2b10031849a5b0
vadimbujorean <vadim03@tiscali.it>
2022-12-01 12:40:17 +0100

    test

--- Challenge1/Message.cs
+++ Challenge1/Message.cs
@@ -1,21 +1,18 @@
 ﻿using Azure.Storage.Queues;
 using System.Collections;
+using System.Text.Json.Serialization;
 
 namespace Challenge1
 {
     public class Message
     {
-        public string DateTime { get; set; }
+        [JsonIgnore]
+        public DateTime DateTime { get; set; } = DateTime.Now;
         
         public string MessageTitle { get; set; }
 
         public string MessageBody { get; set; }
 
         public string Sender { get; set; }
-
-        public Message()
-        {
-
-        }
     }
 }
